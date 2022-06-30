1ac8a42ed5ef5511bcf89cb8562d34409b82d5e4
diegosmello2 <108373754+diegosmello2@users.noreply.github.com>
2022-06-30 19:26:24 -0300

    Initial commit

---
+++ ATVDEnterTheMatrix/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace ATVDEnterTheMatrix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtSobrenome = new System.Windows.Forms.TextBox();
+            this.txtAno = new System.Windows.Forms.TextBox();
+            this.pibImagem = new System.Windows.Forms.PictureBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pibImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "VERIFICAÇÃO:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NOME:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "SOBRENOME:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(16, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(171, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ANO DE NASCIMENTO:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(168, 82);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(254, 20);
+            this.txtNome.TabIndex = 4;
+            this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
+            // 
+            // txtSobrenome
+            // 
+            this.txtSobrenome.Location = new System.Drawing.Point(168, 116);
+            this.txtSobrenome.Name = "txtSobrenome";
+            this.txtSobrenome.Size = new System.Drawing.Size(254, 20);
+            this.txtSobrenome.TabIndex = 5;
+            // 
+            // txtAno
+            // 
+            this.txtAno.Location = new System.Drawing.Point(213, 151);
+            this.txtAno.Name = "txtAno";
+            this.txtAno.Size = new System.Drawing.Size(209, 20);
+            this.txtAno.TabIndex = 6;
+            // 
+            // pibImagem
+            // 
+            this.pibImagem.Location = new System.Drawing.Point(494, 52);
+            this.pibImagem.Name = "pibImagem";
+            this.pibImagem.Size = new System.Drawing.Size(422, 276);
+            this.pibImagem.TabIndex = 7;
+            this.pibImagem.TabStop = false;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(490, 240);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(14, 20);
+            this.lblResultado.TabIndex = 8;
+            this.lblResultado.Text = ":";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(19, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "CHECAR:";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(168, 237);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "LIMPAR:";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(330, 198);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "SAIR:";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ATVDEnterTheMatrix.Properties.Resources.F5;
+            this.ClientSize = new System.Drawing.Size(940, 426);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.pibImagem);
+            this.Controls.Add(this.txtAno);
+            this.Controls.Add(this.txtSobrenome);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "EEntaoGoku";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pibImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtSobrenome;
+        private System.Windows.Forms.TextBox txtAno;
+        private System.Windows.Forms.PictureBox pibImagem;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ATVDEnterTheMatrix/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATVDEnterTheMatrix
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pibImagem.Image = Properties.Resources.F3;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Váriaveis para armazenar os campos::
+            string nome;
+            string sobrenome;
+            int ano;
+            int dataAtual;
+
+            // Armazenar os dados:
+            nome = txtNome.Text;
+            sobrenome = txtSobrenome.Text;
+            ano = int.Parse(txtAno.Text);
+            dataAtual = int.Parse(DateTime.Now.ToString("yyyy"));
+
+            // Checar:
+            if (dataAtual - ano >= 18)
+            {
+                // Caso a idade seja aprovada:
+                lblResultado.Text = "Bem vindo, " +
+                nome + " " + sobrenome + " que possui " + (dataAtual - ano) + " anos.";
+
+                lblResultado.ForeColor = Color.Green;
+                pibImagem.Image = Properties.Resources.F1;
+            }
+            else
+            {
+                // Caso a idade seja negada:
+                lblResultado.Text = "Acesso Negado, " +
+                nome + " " + sobrenome + " que possui " + (dataAtual - ano) + " anos.";
+
+                lblResultado.ForeColor = Color.Red;
+                pibImagem.Image = Properties.Resources.F2;
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Comando para sair:
+            Application.Exit();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Comando para limpar:
+            txtNome.Text = null;
+            txtSobrenome.Text = null;
+            txtAno.Text = null;
+            lblResultado.Text = " ";
+
+            pibImagem.Image = Properties.Resources.F3;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
